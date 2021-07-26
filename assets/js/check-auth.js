c93c28c0dc990ea556b55b0296435bcf2659fc69
koppa53 <jeffersonclemente.70@gmail.com>
2021-07-26 23:30:59 +0800

    Added Check Authorized Voter

--- assets/js/check-auth.js
+++ assets/js/check-auth.js
@@ -1,6 +1,6 @@
 
 function checkAuth() {
-    if (sessionStorage.hasOwnProperty('hasLoggedIn')) {
+    if (sessionStorage.hasOwnProperty('User ID') && sessionStorage.hasOwnProperty('College')) {
     } else {
         window.location.href = "auth-login.html"
     }
