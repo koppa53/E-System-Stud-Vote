fd3bf32021837e5201b2e277d31d1552581be875
koppa53 <jeffersonclemente.70@gmail.com>
2021-07-31 15:32:04 +0800

    Update

--- assets/js/submitVerifyVotes.js
+++ assets/js/submitVerifyVotes.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
         if (a === b) return true;
         if (a == null || b == null) return false;
         if (a.length !== b.length) return false;
-        
+
         for (var i = 0; i < a.length; ++i) {
             if (a[i] != "pass") {
                 if (b[i] > 1) {
